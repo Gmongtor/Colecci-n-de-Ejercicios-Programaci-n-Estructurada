491fa76d5b72dbf807bd647e0203d499a457d204
Gmongtor <gmongtor@myuax.com>
2023-12-12 20:21:18 +0100

    commit probamos el codigo

--- Ejercicio 5/main.cpp
+++ Ejercicio 5/main.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 typedef struct {
     char nombre[100];
@@ -8,5 +9,17 @@ typedef struct {
 
 void imprimirEstudiantePorReferencia(Estudiante *est) {
     printf("Nombre: %s\nEdad: %d\nPromedio: %.2f\n", est->nombre, est->edad, est->promedio);
+}
+
+int main() {
+
+    Estudiante estudiante1;
+    strcpy(estudiante1.nombre, "Sergio");
+    estudiante1.edad = 16;
+    estudiante1.promedio = 5.5;
+
+    imprimirEstudiantePorReferencia(&estudiante1);
+
+    return 0;
 }
 
