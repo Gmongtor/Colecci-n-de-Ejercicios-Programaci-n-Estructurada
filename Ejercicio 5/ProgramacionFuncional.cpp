46d83b1cd815bc5e777879376cf51a1e692ac216
Gmongtor <gmongtor@myuax.com>
2023-12-12 20:24:59 +0100

    commit public

--- Ejercicio 5/ProgramacionFuncional.cpp
+++ Ejercicio 5/ProgramacionFuncional.cpp
@@ -7,5 +7,10 @@ private:
     std::string nombre;
     int edad;
     float promedio;
+public:
+    Estudiante(std::string n, int e, float p) : nombre(n), edad(e), promedio(p) {}
 
+    void imprimir() {
+        std::cout << "Nombre: " << nombre << "\nEdad: " << edad << "\nPromedio: " << promedio << std::endl;
+    }
 };
